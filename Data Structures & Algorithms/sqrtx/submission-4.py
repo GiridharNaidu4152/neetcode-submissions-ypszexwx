ba1861087d5b316cbcc0606ee5aab1951dd63800
Dabbara Giridhar Naidu <giridhar91269@gmail.com>
2026-04-24 05:35:50 +0530

    Add: sqrtx - submission-4

---
+++ Data Structures & Algorithms/sqrtx/submission-4.py
@@ -0,0 +1,14 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x<2:
+            return x
+        l,r=1,x//2
+        while l<=r:
+            mid=(l+r)//2
+            if mid**2<x:
+                l=mid+1
+            elif mid**2>x:
+                r=mid-1
+            else:
+                return mid
+        return r
