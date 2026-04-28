e97d21fcdb4ccb9120c4506bdd65f46182f3d348
Dabbara Giridhar Naidu <giridhar91269@gmail.com>
2026-04-28 21:45:21 +0530

    Add: baseball-game - submission-1

---
+++ Data Structures & Algorithms/baseball-game/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        stk=[]
+        for char in operations:
+            if char[0]=="-":
+                stk.append(-(int(char[1:])))
+            elif char.isdecimal():
+                stk.append(int(char))
+            elif char=="C":
+                stk.pop()
+            elif char=="D":
+                stk.append(2*int(stk[-1]))
+            elif char=="+":
+                stk.append(stk[-1]+stk[-2])
+        return sum(stk)            
