9cbb07414128f967fbaf927e33ac9e4dbf4cca1c
Dabbara Giridhar Naidu <giridhar91269@gmail.com>
2026-08-28 20:40:17 +0530

    Add: add-two-numbers - submission-1

---
+++ Data Structures & Algorithms/add-two-numbers/submission-1.py
@@ -0,0 +1,54 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        head=ListNode()
+        temp=head
+        carry=0
+        while l1 and l2:
+            add=l1.val+l2.val+carry
+            if add>=10:
+                carry=1
+                temp.next=ListNode(add-10)
+                l1=l1.next
+                l2=l2.next
+                temp=temp.next
+            else:
+                carry=0
+                temp.next=ListNode(add)
+                l1=l1.next
+                l2=l2.next
+                temp=temp.next
+        while l1:
+            add=l1.val+carry
+            if add>=10:
+                carry=1
+                temp.next=ListNode(add-10)
+                l1=l1.next
+                temp=temp.next
+            else:
+                carry=0
+                temp.next=ListNode(add)
+                l1=l1.next
+                temp=temp.next
+        while l2:
+            add=l2.val+carry
+            if add>=10:
+                carry=1
+                temp.next=ListNode(add-10)
+                l2=l2.next
+                temp=temp.next
+            else:
+                carry=0
+                temp.next=ListNode(add)
+                l2=l2.next
+                temp=temp.next
+        if carry==1:
+            temp.next=ListNode(1)
+
+        
+        return head.next
